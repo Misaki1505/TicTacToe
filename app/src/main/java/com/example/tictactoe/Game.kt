92867af797d16f7e163100877e487d1e3303b1ae
Misaki1505 <sawako1505@gmail.com>
2022-12-13 18:48:04 -0500

    Changes 13/12/2022

---
+++ app/src/main/java/com/example/tictactoe/Game.kt
@@ -0,0 +1,28 @@
+package com.example.tictactoe
+
+var board = arrayListOf<ArrayList<String>>()
+
+fun main(args: Array<String>) {
+    for(i in 0..2) {
+        val row = arrayListOf<String>()
+        for(j in 0..2)
+            row.add("")
+        board.add(row)
+    }
+    printBoard()
+}
+
+fun printBoard(){
+    println("----------------")
+    for(i in 0..2) {
+        for(j in 0..2) {
+            when(board[i][j]) {
+                "x" -> print("| x ")
+                "o" -> print("| o ")
+                else -> print("|   ")
+            }
+        }
+        println("|")
+        println("---------------")
+    }
+}
